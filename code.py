a0316dc72860072c5e3f0957e58dac24514c7f1a
dntoth-dev <toth.daniel@e5vos.hu>
2025-11-21 20:22:42 +0100

    fetch and store routes from .csv
    One list stores all routes, with each list element being a route. The code includes element logic, which filters against empty cells.

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ airports = "airports_short.csv"
 fixes = "fixes_short.csv"
 routes = "routes.csv"
 
-
+# region Fetching airport coordinates from the .csv file by entering the airport ICAO identifier
 def fetch_airport_coordinates(icao):
     with open(airports, "r") as file:
         next(file)
@@ -17,7 +17,9 @@ def fetch_airport_coordinates(icao):
                 break
             else:
                 continue
+# endregion
 
+# region Fetching the coordinates of fixes from the .csv file by entering their unique, 5 letter identifier
 def fetch_fix_coordinates(id):
     with open(fixes, "r") as file:
         next(file)
@@ -30,3 +32,19 @@ def fetch_fix_coordinates(id):
                 break
             else:
                 continue
+# endregion
+
+# One list will store all the routes, each list element is a route
+default_routes = []
+
+# Opening routes.csv, going through elements of routes. Includes element logic, which filters against empty cells
+with open(routes, "r") as file:
+    next(file)
+    for line in file:
+        current_route = []
+        parts = line.strip().split(';')
+        for piece in parts:
+            if piece.isalpha(): # Exclude empty cells
+                current_route.append(piece)
+        default_routes.append(current_route)
+        
