2c6da058b64645f9db125a1574975a5c743f678b
dntoth-dev <toth.daniel@e5vos.hu>
2025-12-05 23:17:05 +0100

    change to general coordinate fetching
    This is a major and probably the last update of the coordinate fetching function. I changed the code in a way that if you enter the id of an airport or NAVAID, the program checks if it is 4 characters long (ICAO) or 5 characters long (NAVAID), and based on this, it will open the necessary file, airports_short.csv or fixes_short.csv. This saved many lines of code, and brought the program to a simpler level, saving memory and other resources.

--- code.py
+++ code.py
@@ -12,76 +12,84 @@ airports = "airports_short.csv"
 fixes = "fixes_short.csv"
 routes = "routes.csv"
 
-# region Fetching airport coordinates from the .csv file by entering the airport ICAO identifier
-def fetch_airport_lat(icao):
-    with open(airports, "r") as file:
-        next(file)
-        for line in file:
-            parts = line.strip().split(';')
-            if parts[0] == icao.upper():
-                lat = parts[9]
-                return lat
-            else:
-                continue
-            
-def fetch_airport_long(icao):
-    with open(airports, "r") as file:
-        next(file)
-        for line in file:
-            parts = line.strip().split(';')
-            if parts[0] == icao.upper():
-                long = parts[10]
-                return long
-            else:
-                continue
-# endregion
+# region Fetch latitude of airports/navaids
+def lat(id):
+    if len(id) == 4: # Airports have a 4 letter identifier (luckily that's an easy difference to spot lets goo)
+        icao = id
+        with open(airports, "r") as file:
+            next(file)
+            for line in file:
+                parts = line.strip().split(';')
+                if parts[0] == icao.upper():
+                    lat = parts[9]
+                    return lat
+                else:
+                    continue
+    elif len(id) == 5: # Fixes have a 5 letter identifier
+        navaid = id
+        with open(fixes, "r") as file:
+            next(file)
+            for line in file:
+                parts = line.strip().split(';')
+                if parts[0] == navaid.upper():
+                    lat = parts[1]
+                    # A formula converting DMS (Degrees, Minutes, Seconds) to DD (Decimal Degrees) format, because in the fixes_short.csv, coordinates are provided like that
+                    prefix = ""
+                    if lat[0] == 'S':
+                        prefix = '-'
+                    if lat[1] != 0:
+                        deg = lat[1] + lat[2]
+                    else:
+                        deg = lat[2]
+                    min = lat[3] + lat[4]
+                    sec = lat[5] + lat[6]
+                    dec_deg = int(deg) + int(min)/60 + int(sec)/3600
+                    lat_dec_deg = f"{prefix}{dec_deg}"
+                    return lat_dec_deg
+                else:
+                    continue
+    else:
+        print("Please enter a 4 letter ICAO or a 5 letter NAVAID!\nNOTE: THE PROGRAM WORKS FOR EUROPEAN ROUTES ONLY!")
 
-# region Fetching the coordinates of fixes from the .csv file by entering their unique, 5 letter identifier
-def fetch_fix_lat(id):
-    with open(fixes, "r") as file:
-        next(file)
-        for line in file:
-            parts = line.strip().split(';')
-            if parts[0] == id.upper():
-                lat = parts[1]
-                # A formula converting DMS (Degrees, Minutes, Seconds) to DD (Decimal Degrees) format, because in the fixes_short.csv, coordinates are provided like that
-                prefix = ""
-                if lat[0] == 'S':
-                    prefix = '-'
-                if lat[1] != 0:
-                    deg = lat[1] + lat[2]
+# endregion
+# region Fetch longitude of airports/navaids
+def long(id):
+    if len(id) == 4: # Airports have a 4 letter identifier (luckily that's an easy difference to spot lets goo)
+        icao = id
+        with open(airports, "r") as file:
+            next(file)
+            for line in file:
+                parts = line.strip().split(';')
+                if parts[0] == icao.upper():
+                    long = parts[10]
+                    return long
                 else:
-                    deg = lat[2]
-                min = lat[3] + lat[4]
-                sec = lat[5] + lat[6]
-                dec_deg = int(deg) + int(min)/60 + int(sec)/3600
-                lat_dec_deg = f"{prefix}{dec_deg}"
-                return lat_dec_deg
-            else:
-                continue
-            
-def fetch_fix_long(id):
-    with open(fixes, "r") as file:
-        next(file)
-        for line in file:
-            parts = line.strip().split(';')
-            if parts[0] == id.upper():
-                long = parts[2]
-                # A formula converting DMS (Degrees, Minutes, Seconds) to DD (Decimal Degrees) format, because in the fixes_short.csv, coordinates are provided like that
-                prefix = ""
-                if long[0] == 'W':
-                    prefix = '-'
-                if long[1] != 0:
-                    deg = long[1] + long[2] + long[3]
+                    continue
+    elif len(id) == 5: # Fixes have a 5 letter identifier
+        navaid = id
+        with open(fixes, "r") as file:
+            next(file)
+            for line in file:
+                parts = line.strip().split(';')
+                if parts[0] == navaid.upper():
+                    long = parts[2]
+                    # A formula converting DMS (Degrees, Minutes, Seconds) to DD (Decimal Degrees) format, because in the fixes_short.csv, coordinates are provided like that
+                    prefix = ""
+                    if long[0] == 'W':
+                        prefix = '-'
+                    if long[1] != 0:
+                        deg = long[1] + long[2] + long[3]
+                    else:
+                        deg = long[2] + long[3]
+                    min = long[4] + long[5]
+                    sec = long[6] + long[7]
+                    dec_deg = int(deg) + int(min)/60 + int(sec)/3600
+                    long_dec_deg = f"{prefix}{dec_deg}"
+                    return long_dec_deg
                 else:
-                    deg = long[2] + long[3]
-                min = long[4] + long[5]
-                sec = long[6] + long[7]
-                dec_deg = int(deg) + int(min)/60 + int(sec)/3600
-                long_dec_deg = f"{prefix}{dec_deg}"
-                return long_dec_deg
-            else:
-                continue
+                    continue
+    else:
+        print("Please enter a 4 letter ICAO or a 5 letter NAVAID!\nNOTE: THE PROGRAM WORKS FOR EUROPEAN ROUTES ONLY!")
 # endregion
 
 # One list will store all the routes, each list element is a route
@@ -107,7 +115,8 @@ for route in default_routes:
     
 EARTH_RADIUS_KM = 6371.0088
     
-def haversine_distance_wgs84(lat1, lon1, lat2, lon2):
+# Distance calculation with this function (original name: haversine_distance_wgs84)
+def haversine(lat1, lon1, lat2, lon2):
     """
     Calculates the great-circle distance between two points 
     (given in decimal degrees) on the surface of the Earth using 
@@ -141,6 +150,7 @@ def haversine_distance_wgs84(lat1, lon1, lat2, lon2):
     
     return distance    
 
+# Calculate aircraft bearing with this function
 def calculate_bearing(lat1, lon1, lat2, lon2):
     lat1_rad = math.radians(lat1)
     lon1_rad = math.radians(lon1)
